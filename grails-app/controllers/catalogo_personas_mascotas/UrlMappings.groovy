50ae46751113d9acc648c92c8f23a14adb9f0f5c
Alondra <alondrav358@gmail.com>
2020-03-10 12:57:28 -0600

    Update UrlMappings.groovy

--- grails-app/controllers/catalogo_personas_mascotas/UrlMappings.groovy
+++ grails-app/controllers/catalogo_personas_mascotas/UrlMappings.groovy
@@ -9,6 +9,10 @@ class UrlMappings {
             }
         }
 
+        get             "/api/people"(controller: 'personRest', action: 'index')
+        get             "/api/people/$id?"(controller: 'personRest', action: 'show')
+        post            "/api/people/$id?/pets"(controller: 'personRest', action: 'save')
+
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
